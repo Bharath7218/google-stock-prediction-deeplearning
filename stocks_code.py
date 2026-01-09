3152d9b7f281a087c27afff6b6b7650f58bc47f6
Bharath Kumar <bharathsiripurapu21621@gmail.com>
2026-01-10 00:33:38 +0530

    Update stocks_code.py

--- stocks_code.py
+++ stocks_code.py
@@ -40,7 +40,7 @@ Project code is more longer with detailed description.
 
 If you want Project Code, synopsis and Report then Please mail me at vatshayan007@gmail.com
 
-## Mail me at vatshayan007@gmail.com for the Project files now.
+## Mail me at bharathsiripurapu21621@gmail.com for the Project files now.
 """
 
 
